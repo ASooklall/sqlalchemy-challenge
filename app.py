cf0161f7013082fefe70ffc4a7fcd7c0cff1326c
Andrew Sooklall <andrewsooklall@gmail.com>
2019-10-24 19:48:49 -0400

    completed first route and home

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 # Save reference to the table
-Passenger = Base.classes.passenger
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
 #################################################
 # Flask Setup
@@ -46,41 +47,47 @@ def welcome():
 # Define what to do when a user hits the /about route
 @app.route("/api/v1.0/precipitation")
 def precip():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
 
+    session = Session(engine)
+    results = session.query(Measurement.date, Measurement.prcp).all()
+    session.close()
+    prc_1 = list(np.ravel(results))
 #   * Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
+    all_prc = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        all_prc.append(prcp_dict)
 
 #   * Return the JSON representation of your dictionary.
-    results = session.query(Measurements.name).all()
-
-    session.close()
-
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
-
-    return jsonify(all_names)
+    return jsonify(all_prc)
 
 @app.route("/api/v1.0/stations")
 def stations():
+    return("fred")
 #   * Return a JSON list of stations from the dataset.
 
 @app.route("/api/v1.0/tobs")
 def tobs_obs():
+        return("fred")
 #   * query for the dates and temperature observations from a year from the last data point.
 #   * Return a JSON list of Temperature Observations (tobs) for the previous year.
 
 @app.route("/api/v1.0/<start>")
 def start_only():
-
+    return("fred")
 #   * Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
 
 #   * When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
 
 @app.route("/api/v1.0/<start>/<end>")
 def start_end():
+        return("fred")
 #   * Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
 #   * When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
